8b0009a802cf382f7e7e9b960be9483c89c61269
guilhermeonrails <guilhermelimaruby@gmail.com>
2023-08-07 11:17:50 -0300

    aula_1

---
+++ app.js
@@ -0,0 +1,7 @@
+alert('Boas vindas ao jogo do número secreto');
+let numeroSecreto = 5;
+let chute = prompt('Escolha um número entre 1 e 10');
+
+if (chute == numeroSecreto) {
+    console.log('Isso ai! Você descobriu o número secreto (5)');
+}
