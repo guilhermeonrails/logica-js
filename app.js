638086e281dcbc98417ce2704dfeae1bbca89734
guilhermeonrails <guilhermelimaruby@gmail.com>
2023-08-07 13:29:29 -0300

    aula

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 alert('Boas vindas ao jogo do número secreto');
-let numeroSecreto = 5;
+let numeroSecreto = 9;
+console.log(numeroSecreto)
 let chute = prompt('Escolha um número entre 1 e 10');
 
+// se chute for igual ao número secreto
 if (chute == numeroSecreto) {
-    console.log('Isso ai! Você descobriu o número secreto (5)');
+    alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto}`);
+} else {
+    alert('Você errou :(')
 }
