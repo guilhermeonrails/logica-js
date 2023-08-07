a5d6d2855b0de234dbeb4aaccf3fc983eaffc8ab
guilhermeonrails <guilhermelimaruby@gmail.com>
2023-08-07 15:21:31 -0300

    resolucao do desafio

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 alert('Boas vindas ao jogo do número secreto');
-let numeroSecreto = parseInt(Math.random() * 100 + 1);
+let numeroMaximo = 5000;
+let numeroSecreto = parseInt(Math.random() * numeroMaximo + 1);
 console.log(numeroSecreto);
 let chute;
 let tentativas = 1;
 
 // enquanto chute não for igual ao n.s.
 while (chute != numeroSecreto) {
-    chute = prompt('Escolha um número entre 1 e 100');
+    chute = prompt(`Escolha um número entre 1 e ${numeroMaximo}`);
     // se chute for igual ao número secreto
     if (chute == numeroSecreto) {
         break;
